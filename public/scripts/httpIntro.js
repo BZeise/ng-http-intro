54bbe36e942f73eb57d96313fbc36e1b4e8a1731
Ben Zeise <ben.zeise@gmail.com>
2017-06-15 11:50:44 -0500

    FIXED ERROR WOO

--- public/scripts/httpIntro.js
+++ public/scripts/httpIntro.js
@@ -8,13 +8,19 @@ myApp.controller( 'GiphyController', function( GifService ){
 
   vm.randomGif = function(){
 
-  console.log(GifService.randomGif());
-  GifService.randomGif().then(function(response) {
-    console.log('in GifService.randomGif, response is: ', response);
+    console.log(GifService.randomGif());
+    GifService.randomGif().then(function(response) {
+      console.log('in GifService.randomGif, response is: ', response);
 
-    // set vm.display to the URL of image to show
-    // vm.display = response;
-  });
-};
+      // set vm.display to the URL of image to show
+      vm.display = GifService.theGif;
+    });
+  };
+
+  vm.searchGif = function(){
+    GifService.searchGif(vm.searchInput).then(function(response) {
+      vm.display = GifService.display;
+    });
+  };
 
 }); // end GiphyController
--- public/scripts/services/gif.service.js
+++ public/scripts/services/gif.service.js
@@ -5,22 +5,21 @@ myApp.service('GifService', function($http){
   // find a random gif to return for display
   sv.randomGif = function(){
     console.log( 'in randomGif' );
-    $http.get( 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC' ).then( function( response ){
-      console.log(typeOf(response.data.data.image_url));
+    return $http.get( 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC' ).then( function( response ){
+      // console.log(typeof(response.data.data.image_url));
       sv.theGif = response.data.data.image_url;
-      return sv.theGif;
     });
   }; // end randomGif
 
-  sv.searchGif = function() {
+  sv.searchGif = function(searchFor) {
     // assemble search term from ng-model "searchInput"
     var searchUrl = "http://api.giphy.com/v1/gifs/search?q=";
     // target search input
-    searchUrl += sv.searchInput;
+    searchUrl += searchFor;
     searchUrl += "&api_key=dc6zaTOxFJmzC";
     console.log( 'searching for: ', searchUrl);
 
-    $http.get( searchUrl ).then( function( response ){
+    return $http.get( searchUrl ).then( function( response ){
       console.log( 'response is: ', response);
 
       // since we only display one image...
